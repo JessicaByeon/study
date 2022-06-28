5f84bdfc173b6bd0eabc0908afc24997556442e3
JessicaByeon <107660152+JessicaByeon@users.noreply.github.com>
2022-06-28 22:13:27 +0900

    Add files via upload

--- keras11_kaggle_bike.py
+++ keras11_kaggle_bike.py
@@ -1,4 +1,5 @@
-# 데이콘 따릉이 문제풀이
+# https://www.kaggle.com/competitions/bike-sharing-demand
+# 캐글 바이크 문제풀이
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import Sequential
@@ -7,58 +8,55 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 
 #1. 데이터
-path = './_data/ddarung/' # 경로 = .현재폴더 /하단
+path = './_data/kaggle_bike/' # 경로 = .현재폴더 /하단
 train_set = pd.read_csv(path + 'train.csv', # train.csv 의 데이터가 train set에 들어가게 됨
                         index_col=0) # 0번째 컬럼은 인덱스로 지정하는 명령
 print(train_set)
-print(train_set.shape) # (1459, 10) 원래 열이 11개지만, id를 인덱스로 제외하여 10개
+print(train_set.shape) # (10886, 11)
 
 test_set = pd.read_csv(path + 'test.csv',
                        index_col=0)
-submission = pd.read_csv(path + 'submission.csv',
+sampleSubmission = pd.read_csv(path + 'sampleSubmission.csv',
                        index_col=0)
 print(test_set)
-print(test_set.shape) # (715, 9) # 예측 과정에서 쓰일 예정
+print(test_set.shape) # (6493, 8) # 예측 과정에서 쓰일 예정
 
 print(train_set.columns)
 print(train_set.info()) # 각 컬럼에 대한 디테일한 내용 출력 / null값(중간에 빠진 값) '결측치'
 print(train_set.describe())
 
-# 결측치가 있어 데이터를 계산 시 nan/null 값이 되므로 결측치를 삭제 해 주면 됨
-# 결측치 삭제를 위해 같은 행을 삭제해버릴경우, 해당 행의 기존에 있던 데이터 들이 사라지므로 위험 
-
 #### 결측치 처리 1. 제거 ####
 print(train_set.isnull().sum()) # 각 컬럼당 null의 갯수 확인가능
-train_set = train_set.fillna(train_set.mean()) # nan 값을 행별로 모두 삭제(dropna)
+train_set = train_set.fillna(train_set.mean())
 print(train_set.isnull().sum())
-print(train_set.shape) # (1328, 10) 데이터가 얼마나 삭제된 것인지 확인가능 1459-1328=131개 삭제
+print(train_set.shape) # (10886, 11)
 
 
 test_set = test_set.fillna(test_set.mean())
 
 
-x = train_set.drop(['count'], axis=1) # axis는 'count'가 컬럼이라는 것을 명시하기 위해
+x = train_set.drop(['casual', 'registered', 'count'], axis=1) # axis는 'count'가 컬럼이라는 것을 명시하기 위해
 print(x)
 print(x.columns)
-print(x.shape) # (1459, 9)
+print(x.shape) # (10886, 8)
 
 y = train_set['count']
 print(y)
-print(y.shape) # (1459,)
+print(y.shape) # (10886, )
 
 x_train, x_test, y_train, y_test = train_test_split(x, y,
-        train_size=0.98, shuffle=True, random_state=68)
+        train_size=0.99, shuffle=True, random_state=68)
 
 #2. 모델구성
 model = Sequential()
-model.add(Dense(100, input_dim=9))
+model.add(Dense(100, input_dim=8))
 model.add(Dense(100, activation='selu'))
 model.add(Dense(100, activation='selu'))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
 model.compile(loss='mae', optimizer='adam')
-model.fit(x_train, y_train, epochs=530, batch_size=22)
+model.fit(x_train, y_train, epochs=330, batch_size=100)
 
 #4. 평가 예측
 loss = model.evaluate(x_test, y_test)
@@ -67,7 +65,7 @@ print('loss :', loss)
 y_predict = model.predict(x_test)
 
 def RMSE(y_test, y_predict) : #(원y값, 예측y값)
-    return np.sqrt(mean_squared_error(y_test, y_predict)) # MSE에 루트를 씌워 돌려주겠다.
+    return np.sqrt(mean_squared_error(y_test, y_predict))
 
 rmse = RMSE(y_test, y_predict)
 print("RMSE : ", rmse)
@@ -75,16 +73,14 @@ print("RMSE : ", rmse)
 
 y_summit = model.predict(test_set)
 
-# print(y_summit)
-# print(y_summit.shape) # (715, 1)
+print(y_summit)
+print(y_summit.shape)
 
 
-# .to_csv()를 사용하여
-# submission.csv를 완성하시오
+sampleSubmission['count'] = y_summit
+sampleSubmission = abs(sampleSubmission)
+sampleSubmission = sampleSubmission.fillna(sampleSubmission.mean())
+sampleSubmission.to_csv(path + 'sampleSubmission_test04.csv', index=True)
 
-submission['count'] = y_summit
-submission = submission.fillna(submission.mean())
-submission.to_csv('test5.csv', index=True)
-
-# loss : 16.401729583740234
-# RMSE :  19.031272227452
+# loss : 92.75188446044922
+# RMSE :  139.62974866131475
